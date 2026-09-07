744517de5dce3503251f8efc40db3994d9987d4f
adityakashyap7047 <dhruve123456789@gmail.com>
2026-09-07 15:17:04 +0530

    Update /msg to send embed

--- src/commands.js
+++ src/commands.js
@@ -541,15 +541,23 @@ const commands = [
     }
   },
   {
-    data: new SlashCommandBuilder().setName('msg').setDescription('Send a message to a channel')
+    data: new SlashCommandBuilder().setName('msg').setDescription('Send an embed message to a channel')
       .addChannelOption(o => o.setName('channel').setDescription('Target channel').addChannelTypes(ChannelType.GuildText).setRequired(true))
-      .addStringOption(o => o.setName('message').setDescription('Message to send').setRequired(true))
+      .addStringOption(o => o.setName('title').setDescription('Embed title').setRequired(true))
+      .addStringOption(o => o.setName('message').setDescription('Embed description').setRequired(true))
+      .addStringOption(o => o.setName('color').setDescription('Embed color (hex)').setRequired(false))
+      .addStringOption(o => o.setName('footer').setDescription('Embed footer').setRequired(false))
       .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
     async execute(interaction) {
       const channel = interaction.options.getChannel('channel');
+      const title = interaction.options.getString('title');
       const message = interaction.options.getString('message');
-      await channel.send({ content: message });
-      await interaction.reply({ content: `Message sent to ${channel}!`, ephemeral: true });
+      const color = interaction.options.getString('color') || '#00ff88';
+      const footer = interaction.options.getString('footer');
+      const embed = new EmbedBuilder().setColor(color).setTitle(title).setDescription(message).setTimestamp();
+      if (footer) embed.setFooter({ text: footer });
+      await channel.send({ embeds: [embed] });
+      await interaction.reply({ content: `Embed sent to ${channel}!`, ephemeral: true });
     }
   },
   {
